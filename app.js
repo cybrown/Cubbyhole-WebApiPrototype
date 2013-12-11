147047d94a6098cd74c130f8ca266d436e27acbe
Cy Brown <cy@cybrown.fr>
2013-12-11 15:32:30 +0100

    Remplacement urlencoded par bodyParser

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 
 // development only
