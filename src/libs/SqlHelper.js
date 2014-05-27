5956d28a83775b75cb6410af9ba110a8040cbb84
Cy Brown <cy@cybrown.fr>
2014-05-27 21:30:11 +0200

    Correction sql helper

--- src/libs/SqlHelper.js
+++ src/libs/SqlHelper.js
@@ -50,7 +50,7 @@ SqlHelper.prototype.querySelectBy = function (col_name, col_value) {
 SqlHelper.prototype.querySelectBy2 = function (col1_name, col1_value, col2_name, col2_value) {
     var _this = this;
     return Q.promise(function (resolve, reject) {
-        _this.connection.query(SqlHelper.QUERY_SELECT_BY, [_this.PK_NAME, _this.TABLE_FIELDS, _this.TABLE_NAME, col1_name, col1_value, col2_name, col2_value], function (err, result) {
+        _this.connection.query(SqlHelper.QUERY_SELECT_BY_2, [_this.PK_NAME, _this.TABLE_FIELDS, _this.TABLE_NAME, col1_name, col1_value, col2_name, col2_value], function (err, result) {
             _this.log(this.sql);
             err ? reject(err) : resolve(result);
         });
